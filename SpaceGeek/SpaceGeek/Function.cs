d055610dd2ab6ac4eb5acd9263aebeb13ce99cc3
Daniel Larner <daniel.larner@outlook.com>
2018-01-21 10:13:40 -0700

    updates to Function.cs class

--- SpaceGeek/SpaceGeek/Function.cs
+++ SpaceGeek/SpaceGeek/Function.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Alexa.NET.Response;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -48,9 +52,71 @@ namespace SpaceGeek
       return resource.Facts[r.Next(resource.Facts.Count)];
     }
 
-    public string FunctionHandler(string input, ILambdaContext context)
+    public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
     {
-      return input?.ToUpper();
+      SkillResponse response = new SkillResponse();
+      response.Response = new ResponseBody();
+      response.Response.ShouldEndSession = false;
+      IOutputSpeech innerResponse = null;
+      var log = context.Logger;
+      log.LogLine($"Skill Request Object:");
+      log.LogLine(JsonConvert.SerializeObject(input));
+
+      var allResources = GetResources();
+      var resource = allResources.FirstOrDefault();
+
+      if (input.GetRequestType() == typeof(LaunchRequest))
+      {
+        log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
+        innerResponse = new PlainTextOutputSpeech();
+        (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, true);
+
+      }
+      else if (input.GetRequestType() == typeof(IntentRequest))
+      {
+        var intentRequest = (IntentRequest)input.Request;
+        switch (intentRequest.Intent.Name)
+        {
+          case "AMAZON.CancelIntent":
+            log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+            response.Response.ShouldEndSession = true;
+            break;
+          case "AMAZON.StopIntent":
+            log.LogLine($"AMAZON.StopIntent: send StopMessage");
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+            response.Response.ShouldEndSession = true;
+            break;
+          case "AMAZON.HelpIntent":
+            log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+            break;
+          case "GetFactIntent":
+            log.LogLine($"GetFactIntent sent: send new fact");
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
+            break;
+          case "GetNewFactIntent":
+            log.LogLine($"GetFactIntent sent: send new fact");
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
+            break;
+          default:
+            log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+            innerResponse = new PlainTextOutputSpeech();
+            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+            break;
+        }
+      }
+
+      response.Response.OutputSpeech = innerResponse;
+      response.Version = "1.0";
+      log.LogLine($"Skill Request Object...");
+      log.LogLine(JsonConvert.SerializeObject(response));
+      return response;
     }
   }
 
@@ -59,7 +125,7 @@ namespace SpaceGeek
     public FactResource(string language)
     {
       this.Language = language;
-      this.Facts = new List();
+      this.Facts = new List<string>();
     }
 
     public string Language { get; set; }
